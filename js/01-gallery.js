8f60d1e8698ce19797e5ff9133f7434c7895f088
MariaStreltsova <Mariastreltova@gmail.com>
2022-04-21 00:00:50 +0300

    creates gallery items

---
+++ js/01-gallery.js
@@ -0,0 +1,27 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryItemsContainer = document.querySelector(".gallery");
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+console.log (galleryItemsContainer.insertAdjacentHTML("beforeend", galleryItemsMarkup));
+console.log(galleryItemsMarkup)
+
+console.log(createGalleryItemsMarkup(galleryItems))
+function createGalleryItemsMarkup(galleryItems) {
+    
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    }).join("");
+
+
+// console.log(markup)
+}
+console.log(galleryItems);
