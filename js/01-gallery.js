284b185b866d11e99ff1b4e256c365909d6c30f6
MariaStreltsova <Mariastreltova@gmail.com>
2022-04-21 11:47:51 +0300

    gallery 01 without esc

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,12 +2,13 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryItemsContainer = document.querySelector(".gallery");
 const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
-console.log (galleryItemsContainer.insertAdjacentHTML("beforeend", galleryItemsMarkup));
-console.log(galleryItemsMarkup)
+galleryItemsContainer.insertAdjacentHTML("beforeend", galleryItemsMarkup);
 
-console.log(createGalleryItemsMarkup(galleryItems))
-function createGalleryItemsMarkup(galleryItems) {
-    
+
+galleryItemsContainer.addEventListener('click', onClickGalleryItems)
+
+// console.log(createGalleryItemsMarkup(galleryItems))
+function createGalleryItemsMarkup(items) {
     return galleryItems.map(({ preview, original, description }) => {
         return `<div class="gallery__item">
   <a class="gallery__link" href="${original}">
@@ -24,4 +25,25 @@ function createGalleryItemsMarkup(galleryItems) {
 
 // console.log(markup)
 }
+function onClickGalleryItems(event) {
+    event.preventDefault();
+    const galleryImageEl = event.target.classList.contains("gallery__image");
+    if (!galleryImageEl) {
+        return;
+    }
+    console.log(event.target.dataset.source);
+    const urlSource = event.target.dataset.source;
+    createModal(urlSource);
+}
+function createModal(x) {
+    const instance = basicLightbox.create(`
+    <div class="modal">
+<img src="${x}"/>
+    </div>
+`)
+
+instance.show()
+}
+
+
 console.log(galleryItems);
